7404336f12bb6ed2c7087cf7b817aabbf3567b21
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-10-03 11:16:12 +0200

    Dividido en subcomponentes

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [],
+  imports: [CatalogComponent],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit {
