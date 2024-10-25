11bc104a51902f56b205cdbe3098fdbd09babcb5
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-10-25 11:26:34 +0200

    Function reducer escrita

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -31,7 +31,7 @@ export class CartAppComponent implements OnInit {
   ngOnInit(): void {
 
     this.items = JSON.parse(sessionStorage.getItem('cart') || '[]') || []; //Carga el items de la sesion
-    this.calculateTotal(); //Calcula el total al inicio de la aplicacion
+    //this.calculateTotal(); //Calcula el total al inicio de la aplicacion
 
     //Para que se suscriba al inicio ya que tienen la funcion subscribe
     this.onDeleteCart();
@@ -40,39 +40,16 @@ export class CartAppComponent implements OnInit {
 
   onAddToCart(): void {
     this.sharingDataService.productEventEmitter.subscribe(product => {
-      const hasItem = this.items.find(item => item.product.id === product.id);
 
-      if (hasItem) {
-        /* 1
-        hasItem.quantity++;
-        */
-
-        // 2
-        this.items = this.items.map(item => {
-          if (item.product.id === product.id) {
-            //item.quantity++; tambien funciona pero modifica el array original
-            return { ...item, quantity: item.quantity + 1 };
-          }
-          return item;
-        });
-      } else {
-        this.items = [... this.items, { product: { ...product }, quantity: 1 }];
-      }
       //Lo mantenmos aqui ya que si la modal no se abre, no se guardan en la sesion las cosas con el onChange
       //así cada vez que se añada algo al carrito ya se guarda en sesion
       this.saveSession();
-      this.calculateTotal();//Para que se calcule el total despues de que se añada algun producto
+      //this.calculateTotal();//Para que se calcule el total despues de que se añada algun producto
       //this.saveSession();
+
       this.router.navigate(['/cart'], {
         state: { items: this.items, total: this.total }
       });
-      /* 
-      Se copia items para que la lista original no se vea afectada y ya cogemos 
-       esa copia y le añadimos un objeto product que tiene la info del producto 
-       copiada y la cantidad
-       */
-
-
       /*
       Con ... queda así, como un solo array normal: 
       this.items = [
@@ -88,7 +65,6 @@ export class CartAppComponent implements OnInit {
         { product: { name: 'Tablet', price: 300 }, quantity: 1 }
       ];
       */
-
       Swal.fire({
         title: "Shopping Cart",
         text: "Nuevo producto agregado",
@@ -114,16 +90,7 @@ export class CartAppComponent implements OnInit {
       }).then((result) => {
         if (result.isConfirmed) {
 
-
-          //Si es distinto no pasa, se elimina entonces 
-          this.items = this.items.filter(item => item.product.id !== id);
-
-          //Toca hacer esto ya que si el filter deja el array vacio, no lo tiene en 
-          //cuenta como cambio ya que ya ha estado vacio y siempre se queda un ultimo elemento
-          //pero si es 0 borramos el carrito y ya
-          if (this.items.length === 0) sessionStorage.removeItem('cart');
-
-          this.calculateTotal(); //Para que se calcule el total despues de que se elimine algun producto
+          // this.calculateTotal(); //Para que se calcule el total despues de que se elimine algun producto
           this.saveSession();
 
 
@@ -149,9 +116,6 @@ export class CartAppComponent implements OnInit {
 
   }
 
-  calculateTotal(): void {
-    this.total = this.items.reduce((accumulator, item) => accumulator + item.quantity * item.product.price, 0); //0 es el valor inicial del accumulator
-  }
 
   saveSession(): void {
     //Se tiene que guardar lo   que sea como string
