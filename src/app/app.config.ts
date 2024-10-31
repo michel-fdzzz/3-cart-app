68600aa05990011fb7886c5665e30fd51bb0adbf
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-10-31 12:20:58 +0100

    instalado el devtools de redux con ng add @ngrx/store-devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { itemsReducer } from './store/items.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(
-    {
-      items: itemsReducer
-    })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore({
+        items: itemsReducer
+    }), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
